3a60dc36f4af88eda7e5f4093e9fa64fd5935280
LuanPaganucci <luan.paganucci@gmail.com>
2023-01-27 15:18:09 -0300

    Exercicios de 25/01

--- Fundamentos/3-3_Logica/exercicios_pos-aula.js
+++ Fundamentos/3-3_Logica/exercicios_pos-aula.js
@@ -6,10 +6,26 @@
 // console.log(fatorial)
 
 //2
-let word = "Banana"
-let reverseWord = '';
+// let word = "Banana"
+// let reverseWord = '';
 
-for(let index = 0; index < word.length; index +=1) {
-    reverseWord += word[word.length - 1 - index];
+// for(let index = 0; index < word.length; index +=1) {
+//     reverseWord += word[word.length - 1 - index];
+// }
+// console.log(reverseWord);
+
+//3
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let biggestWord = [0];
+let smallestWord = [0];
+
+for(let index = 0; index < array.length; index +=1) {
+    if (array[index].length > biggestWord.length) {
+        biggestWord = array[index];
+    }
 }
-console.log(reverseWord);
+for(let index = 0; index <array.length; index +=1) {
+    if ()
+}
+console.log(biggestWord);
+console.log(smallestWord);
