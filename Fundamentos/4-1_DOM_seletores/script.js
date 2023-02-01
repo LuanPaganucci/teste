7431138e4f1a48c562797ecc5eec70dec1d9114d
LuanPaganucci <luan.paganucci@gmail.com>
2023-02-01 15:33:04 -0300

    Finalizando 4.1

---
+++ Fundamentos/4-1_DOM_seletores/script.js
@@ -0,0 +1,4 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "blue";
+document.getElementById("page-title").innerText = "Efeito Borboleta";
+document.getElementById("subtitle").innerText = "Alterando subtitulo"; 
